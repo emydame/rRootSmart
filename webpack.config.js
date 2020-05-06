bae59d0a89b30df2a943f4f235b6a3b13000e00c
Nwosu Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-06 06:02:01 +0100

    fix fighting-style: correct style on webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: path.resolve(__dirname, 'src', 'frontend'),
-    output:{
-       path: path.resolve(__dirname, 'dist'),
-       filename: 'bundle.js',
-       publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        historyApiFallback: true
-    },
-    module: {
-        rules :[{
-            test:/\.jsx?/,
-            loader: 'babel-loader'
-        }]
-    }
-} 
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'frontend'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
