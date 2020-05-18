3df293dcee8c8cc67025ab63cd19f2c99d44b04e
emmydame <emygreat1912@yahoo.com>
2020-05-18 10:38:35 +0100

    feat: Content addition

--- src/frontend/src/components/content/Content.js
+++ src/frontend/src/components/content/Content.js
@@ -4,7 +4,29 @@ class Content extends React.Component {
   render() {
     return (
       <>
-        <h1>Content content here</h1>
+    
+      <div class="content">      
+              <div class="row">         
+          <div id="bluecol" class="col-md-4">
+            <div class="classSpacing">
+          People search for this Lorem ipsum dummy copy text using all kinds of names, such as Lorem ipsum, lorem ipsum dolor sit amet, Lorem, dummy text, loren ipsum (yes, spelled wrong), Lorem ipsum sample textipsum loremlorem ipsum sample, Latin copy text, Lorem ipsum text, Latin dummy text, template text, sample text, dummy copy text, Latin sample text, HTML dummy text, Lorem ipsum dummy text, filler text or copy filling text, and many other names.
+          </div>
+          </div>
+        <div id="yellowcol" class="col-md-4">        
+        Some Content.. 
+     
+          </div>
+        <div id="bluecol" class="col-md-4 ">
+          <div class="col-md-12">
+        Some Content.. 
+     </div>
+        
+        </div>
+          
+       
+      </div>
+      </div>
+      
       </>
     );
   }
