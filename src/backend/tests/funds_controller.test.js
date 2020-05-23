fbfb6a8014934797e439fccae994efb11bcabcc3
Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-23 08:35:50 +0100

    chore: add test for invests endpoint

---
+++ src/backend/tests/funds_controller.test.js
@@ -0,0 +1,35 @@
+require("mysql2/node_modules/iconv-lite").encodingExists("foo");
+
+const supertest = require("supertest");
+const { app } = require("../app");
+const fundsControllers = require("../controller/funds.controller");
+
+let apiServer;
+
+describe("create()", () => {
+  beforeAll(async () => {
+    apiServer = supertest(app);
+  });
+  afterAll(async () => {
+    await apiServer.close();
+  });
+  it("should be a function", () => {
+    const res = typeof fundsControllers.create;
+    expect(res).toEqual("function");
+  });
+  it("should not create funds when no data is sent", async () => {
+    const res = await apiServer.post("/invest");
+    expect(res.statusCode).toEqual(400);
+  });
+  it("should create funds when valid data", async () => {
+    const res = await apiServer.post("/invest").send({
+      fundId: 1,
+      organizationId: 1,
+      fundCatId: 2,
+      amount: 2000,
+      status: true,
+      dateInitiated: "12-03-1999"
+    });
+    expect(res.statusCode).toEqual(200);
+  });
+});
