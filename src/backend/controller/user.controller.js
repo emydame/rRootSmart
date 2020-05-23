27e1785e5c4df8e027349dc9c9d331c12bb13ce5
victoryohanna <victoryohanna@gmail.com>
2020-05-23 16:40:34 +0100

    fixed issue

--- src/backend/controller/user.controller.js
+++ src/backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const secret = "serete";
 const db = require("../config/db.config");
 const User = db.users;
 const Userpass = db.userLogin;
