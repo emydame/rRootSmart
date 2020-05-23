177d42a6e170b657a0246b1047d5d27b5b4bf57e
Onyedikachi <onyedikachinwosu@rocketmail.com>
2020-05-23 13:50:46 +0100

    update: add tests for create fund disbursement endpoint

---
+++ src/backend/tests/fundcategory_controller.test.js
@@ -0,0 +1,37 @@
+require("mysql2/node_modules/iconv-lite").encodingExists("foo");
+
+const supertest = require("supertest");
+const { app } = require("../app");
+const fundCatControllers = require("../controller/fundCategory.controller");
+
+let apiServer;
+
+describe("create()", () => {
+  beforeEach(async () => {
+    if(!(apiServer && apiServer.listen)){
+      apiServer = supertest(app);
+    } 
+  });
+  afterEach(async () => {
+    if (apiServer.close){
+      await apiServer.close();
+    } 
+  });
+  it("should be a function", () => {
+    const res = typeof fundCatControllers.create;
+    expect(res).toEqual("function");
+  });
+  it("should not create fund category when no data is sent", async () => {
+    const res = await apiServer.post("/funds/category");
+    expect(res.statusCode).toEqual(500);
+  });
+  it("should make a fund category with valid data", async () => {
+    const res = await apiServer.post("/funds/category").send({
+      fundCatId: 1,
+      categoryName: "Loan",
+      description: "Offer loan for agricultural purposes",
+      createdBy: 3
+    });
+    expect(res.statusCode).toEqual(200);
+  });
+});
---
+++ src/backend/tests/funddisbursement_controller.test.js
@@ -0,0 +1,40 @@
+require("mysql2/node_modules/iconv-lite").encodingExists("foo");
+
+const supertest = require("supertest");
+const { app } = require("../app");
+const fundDisbusControllers = require("../controller/fundDisbursment.controller");
+
+let apiServer;
+
+describe("create()", () => {
+  beforeEach(async () => {
+    if(!(apiServer && apiServer.listen)){
+      apiServer = supertest(app);
+    } 
+  });
+  afterEach(async () => {
+    if (apiServer.close){
+      await apiServer.close();
+    } 
+  });
+  it("should be a function", () => {
+    const res = typeof fundDisbusControllers.create;
+    expect(res).toEqual("function");
+  });
+  it("should not disburse fund  when no data is sent", async () => {
+    const res = await apiServer.post("/disbursements");
+    expect(res.statusCode).toEqual(500);
+  });
+  it("should disburse fund with valid data", async () => {
+    const res = await apiServer.post("/disbursements").send({
+      disbursementId: 2,
+      applicationId: 2,
+      fundId: 1,
+      disbursedBy: 1,
+      bankName: "GTBanks",
+      accountNunber: 1202394004,
+      status: true
+    });
+    expect(res.statusCode).toEqual(200);
+  });
+});
