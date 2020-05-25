4600cbb588b433d1974eb74b0bd64d7335a454ff
elfizzy <abulkhair0800@gmail.com>
2020-05-25 22:11:06 +0100

    admin section modification

--- src/frontend/src/components/dashboard/admin/AdminDashboard.js
+++ src/frontend/src/components/dashboard/admin/AdminDashboard.js
@@ -2,8 +2,11 @@
 /* eslint no-console: "error" */
 
 import React from "react";
+import { Switch, Router, Route, Link } from "react-router-dom";
 import { Layout, Menu, Breadcrumb, Row, Col } from "antd";
-import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined } from "@ant-design/icons";
+import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined,LogoutOutlined,AppstoreOutlined, DollarCircleOutlined } from "@ant-design/icons";
+import InvestorsAndFunding from "./InvestorsAndFunding";
+import SmeAndProjects from "./SmeAndProjects";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
@@ -28,10 +31,13 @@ class AdminDashboard extends React.Component {
         <Sider collapsible collapsed={this.state.collapsed} onCollapse={this.onCollapse} style={{ paddingTop: "60px" }}>
           <div className="logo"></div>
           <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              View Projects
+            <Menu.Item key="1" icon={<AppstoreOutlined />}>
+             <Link to="/admin/smeandprojects"> SMEs/Projects</Link>
             </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}></Menu.Item>
+            <Menu.Item key="2" icon={<DollarCircleOutlined />}>
+            <Link to="/admin/investorsandfunding">Investors/Funding</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<LogoutOutlined/>}> Log Out</Menu.Item>
           </Menu>
         </Sider>
         <Layout className="site-layout">
@@ -42,6 +48,14 @@ class AdminDashboard extends React.Component {
             </Row>
           </Header>
           <Content style={{ margin: "0 16px" }}>
+            <Router history={this.props.history}>
+              <Switch>
+                <Route path="/admin/investorsandfunding" component={InvestorsAndFunding} />
+                <Route path="/admin/smeandprojects" component={SmeAndProjects} />
+              </Switch>
+            </Router>
+          </Content>
+          {/* <Content style={{ margin: "0 16px" }}>
             <Breadcrumb style={{ margin: "16px 0" }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
@@ -49,7 +63,7 @@ class AdminDashboard extends React.Component {
             <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
               Bill is a cat.
             </div>
-          </Content>
+          </Content> */}
         </Layout>
       </Layout>
     );
