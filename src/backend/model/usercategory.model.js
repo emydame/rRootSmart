3858c1aa233e3464eeb2fdd0978622eb51e76aa6
victoryohanna <victoryohanna@gmail.com>
2020-05-25 13:56:59 +0100

    user category model modified

--- src/backend/model/usercategory.model.js
+++ src/backend/model/usercategory.model.js
@@ -18,11 +18,11 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING
       },
       createdAt: {
-        type: DataTypes.STRING
+        type: DataTypes.DATE
       }
     },
     {
-      timestamps: false
+      timestamps: false   
     }
   );
   return UserCategory;
