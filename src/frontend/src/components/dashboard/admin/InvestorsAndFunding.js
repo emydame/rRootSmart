be3128abd30f63308129ead05768a9a391a560d8
elfizzy <abulkhair0800@gmail.com>
2020-05-25 23:14:16 +0100

    Investors and sme table with dummy text

--- src/frontend/src/components/dashboard/admin/InvestorsAndFunding.js
+++ src/frontend/src/components/dashboard/admin/InvestorsAndFunding.js
@@ -9,7 +9,12 @@ export default class InvestorsAndFunding extends Component {
         <div className="invest-fund">
           <h2 style={{textAlign:"center"}}>Investors/Funding</h2>
         </div>
+        <div className="update" style={{textAlign:"center"}}>
+          <h4> *** New Request *** </h4>
+          <p>Kuda Micro finance just signed in as an investor and needs to be verified</p>
+        </div>
         <Table striped bordered hover variant="dark">
+          {/* dummy data to be replaced with real data */}
         <thead>
             <tr>
               <th>Action</th>
@@ -21,27 +26,27 @@ export default class InvestorsAndFunding extends Component {
           <tbody>
             <tr>
               <td>Pending Background Check</td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>15</td>
+              <td>25/05/2020</td>
+              <td> <button style={{background:"grey"}}> View Details</button> </td>
             </tr>
             <tr>
               <td>Checked/Approved</td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>7</td>
+              <td>25/05/2020</td>
+              <td> <button style={{background:"grey"}}> View Details</button> </td>
             </tr>
             <tr>
               <td>Checked/Not Approved</td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>10</td>
+              <td>25/05/2020</td>
+              <td> <button style={{background:"grey"}}> View Details</button> </td>
             </tr>
             <tr>
               <td>Blacklisted</td>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td>3</td>
+              <td>25/05/2020</td>
+              <td> <button style={{background:"grey"}}> View Details</button> </td>
             </tr>
           </tbody>
 
