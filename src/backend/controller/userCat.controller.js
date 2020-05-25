bba69f158e3efc3a8a0eafa216eec366ac295543
victoryohanna <victoryohanna@gmail.com>
2020-05-25 10:02:58 +0100

    modified user category controller

--- src/backend/controller/userCat.controller.js
+++ src/backend/controller/userCat.controller.js
@@ -5,7 +5,6 @@ const UserCategory = db.userCategory;
 exports.create = (req, res) => {
   let requests = {
     userCatId: req.body.userCatId,
-    userId: req.body.userId,
     categoryName: req.body.categoryName,
     description: req.body.description,
     createdBy: req.body.createdBy
@@ -18,11 +17,11 @@ exports.create = (req, res) => {
     // query user category table to check if category already exist
     UserCategory.findOne({ where: { categoryName: requests.categoryName } }).then((data) => {
       if (data) {
-        res.status(401).send({
+        return res.status(401).send({
           message: "Category already exist"
         });
       } else {
-        // create new user category 
+        // create user category
         const userCategory = new UserCategory(requests);
         userCategory
           .save()
@@ -30,11 +29,24 @@ exports.create = (req, res) => {
             res.status(200).send(data);
           })
           .catch((err) => {
-            res.status(500).send({
+            return res.status(500).send({
               message: err.message || "Something wrong while creating the user category profile."
             });
           });
       }
     });
   }
+};
+
+//return all user categories
+exports.findAll = (req, res) => {
+  UserCategory.findAll()
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message
+      });
+    });
 };
