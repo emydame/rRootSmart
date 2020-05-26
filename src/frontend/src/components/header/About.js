a3d609bd1ab1bd98d5ea6fc9c4f2339f688aca6b
emmydame <emygreat1912@yahoo.com>
2020-05-26 06:40:31 +0100

    fix: Update on about page content

--- src/frontend/src/components/header/About.js
+++ src/frontend/src/components/header/About.js
@@ -34,7 +34,7 @@ class About extends React.Component {
       <>
         {/*** About Modal */}
         <Modal
-          size="md"
+          size="lg"
           show={this.props.showModal}
           onHide={this.closeAbout}
           dialogClassName="modal-90w"
@@ -45,7 +45,7 @@ class About extends React.Component {
             <Row>
               <Col className="text-center pr-1">
                 <Modal.Title id="login" className="text-light">
-                  About eazSME
+                  We are eazSME
                 </Modal.Title>
               </Col>
             </Row>
@@ -53,11 +53,70 @@ class About extends React.Component {
           <Modal.Body bsPrefix="modal-body">
             <div>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit enim architecto placeat. Expedita
-                assumenda porro voluptatibus illum cum, quasi, deserunt repellendus optio labore libero facilis
-                consequatur alias placeat itaque iste.
+               We believe most Small and Medium Size Businesses aren't getting the finance they need to start and grow their
+               business. This is why we strive to review your organization's financial history, and provide you with the excellent service
+               you deserve to access funds received from Government and International bodies.
+
               </p>
+              <hr></hr>
+              <Row>
+                <Col>
+                <Modal.Title id="login" className="text-light">
+                WHY eazSME?
+                </Modal.Title>               
+                   <p>
+                   SMEs are a crucial contributor to industry in emerging markets, 
+                   contributing up to 60% of total employment.
+                   However, 200 million SMEs lack access to affordable financial services and credit.                                                          
+                   </p>                 
+                </Col>
+                <Col>
+                <br/>
+                   <p>
+                   So we started eazSME to provide a platform through which SMEs can access investment funds
+                    from potential investors through a transparent and regulated process.                                    
+                   </p>                 
+                </Col>               
+              </Row>
             </div>
+            <hr></hr>
+            <div>
+                 <h5>Meet Our Team</h5 >  
+              <p>
+              We are a skilled team of Business Analyst, Software Developers, Project Managers who identified a gap in financing
+              Small and Medium Business Enterprises and have decided to bridge the gap. Our Mission is to make investement funds 
+              accessible to SMEs and the utilization of the funds monitored and regulated.
+              </p>
+              <hr></hr>
+              <Row>
+                <Col> 
+                <figure>
+                  <img src={"./Emilia.png"} alt=""/>
+                <figcaption>Emilia Anochirionye; Technical Team Lead
+                </figcaption>
+              </figure>             
+                            
+                </Col>
+                <Col>                   
+                <figure>
+                  <img src={"./Emilia.png"} alt=""/>
+                <figcaption>Emilia Anochirionye; Technical Team Lead
+                </figcaption>
+              </figure>                       
+                </Col>
+                <Col>                   
+                <figure>
+                  <img src={"./Emilia.png"} alt=""/>
+                <figcaption>Emilia Anochirionye;
+                   Technical Team Lead
+                </figcaption>
+              </figure>                      
+                </Col> 
+              
+                                         
+              </Row>
+            </div>
+           
           </Modal.Body>
         </Modal>
       </>
