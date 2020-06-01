3e34ea9c0e2c963f18b1dc264bd2e0ba9845436b
lordeFid <agbafid3lis@yahoo.com>
2020-06-01 22:56:48 +0100

    Final fix for the Errors so far

--- src/frontend/src/components/dashboard/sme/user/ProfileDetails.js
+++ src/frontend/src/components/dashboard/sme/user/ProfileDetails.js
@@ -22,7 +22,7 @@ function getBase64(img, callback) {
 function beforeUpload(file) {
   const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
   if (!isJpgOrPng) {
-    message.error('You can only upload JPG/PNG file!');
+  message.error("You can only upload JPG/PNG file");
   }
   const isLt2M = file.size / 1024 / 1024 < 2;
   if (!isLt2M) {
