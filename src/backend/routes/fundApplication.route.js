eaeb2b1652e7b67fcf709ac9a5815697e6bf4d0f
victoryohanna <victoryohanna@gmail.com>
2020-06-09 17:09:34 +0100

    fix shorthand method issues

--- src/backend/routes/fundApplication.route.js
+++ src/backend/routes/fundApplication.route.js
@@ -1,21 +1,15 @@
 const multer = require("multer");
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb){
+  destination(req, file, cb) {
     cb(null, "./uploads");
   },
-  filename: function(req, file, cb) { 
+  filename(req, file, cb) {
     cb(null, new Date().toDateString() + file.originalname);
   }
 });
 
-const upload = multer ({ storage: storage, 
-  limits: {
-      fileSize : 1024 * 1024 * 5
-}
-});
-
-//const upload = multer({ dest: 'uploads/' })
+const upload = multer({ storage: storage });
 
 module.exports = (app) => {
   const fundApplication = require("../controller/fundApplication.controller");
