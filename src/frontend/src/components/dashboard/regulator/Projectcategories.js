447efae8e52c7dfc1e9991e6d749bde0101b6d06
elfizzy <abulkhair0800@gmail.com>
2020-06-12 22:10:02 +0100

    update investor/sme list to get data from db

--- src/frontend/src/components/dashboard/regulator/Projectcategories.js
+++ src/frontend/src/components/dashboard/regulator/Projectcategories.js
@@ -2,6 +2,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Card from "react-bootstrap/Card";
+import axios from "axios";
 import { Table, Tag, Space } from "antd";
 
 /*datasource*/
